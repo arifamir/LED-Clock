556e2ec8bba535a1a4229b7179a969b74ca9d547
Arif Amir <arifamirk@gmail.com>
2021-07-24 23:51:04 +0200

    Led retro clock in action

--- main.go
+++ main.go
@@ -2,12 +2,17 @@ package main
 
 import (
 	"fmt"
+	"time"
+
+	"github.com/arifamir/goscreen"
 )
 
 func main() {
 	// for keeping things easy to read and type-safe
 	type placeholder [5]string
 
+	// put the digits (placeholders) into variables
+	// using the placeholder array type above
 	zero := placeholder{
 		"███",
 		"█ █",
@@ -88,6 +93,14 @@ func main() {
 		"███",
 	}
 
+	colon := placeholder{
+		"   ",
+		" ░ ",
+		"   ",
+		" ░ ",
+		"   ",
+	}
+
 	// This array's type is "like": [10][5]string
 	//
 	// However:
@@ -102,18 +115,57 @@ func main() {
 		zero, one, two, three, four, five, six, seven, eight, nine,
 	}
 
-	// Explanation: digits[0]
-	// + Each element of clock has the same length.
-	// + So: Getting the length of only one element is OK.
-	// + This could be: "zero" or "one" and so on... Instead of: digits[0]
-	//
-	// The range clause below is ~equal to the following code:
-	// line := 0; line < 5; line++
-	for line := range digits[0] {
-		// Print a line for each placeholder in digits
-		for digit := range digits {
-			fmt.Print(digits[digit][line], "  ")
+	// For Go Playground, do not use this.
+	goscreen.Clear()
+
+	// Go Playground will not run an infinite loop.
+	// Loop for example 1000 times instead, like this:
+	//   for i := 0; i < 1000; i++ { ... }
+	for {
+		// For Go Playground, use this instead:
+		// fmt.Print("\f")
+		goscreen.MoveTopLeft()
+
+		// get the current hour, minute and second
+		now := time.Now()
+		hour, min, sec := now.Hour(), now.Minute(), now.Second()
+
+		// extract the digits: 17 becomes, 1 and 7 respectively
+		clock := [...]placeholder{
+			digits[hour/10], digits[hour%10],
+			colon,
+			digits[min/10], digits[min%10],
+			colon,
+			digits[sec/10], digits[sec%10],
 		}
-		fmt.Println()
+
+		// Explanation: clock[0]
+		// + Each element of clock has the same length.
+		// + So: Getting the length of only one element is OK.
+		// + This could be: "zero" or "one" and so on... Instead of: digits[0]
+		//
+		// The range clause below is ~equal to the following code:
+		//   line := 0; line < len(clock[0]); line++
+		for line := range clock[0] {
+			// Print a line for each placeholder in clock
+			for index, digit := range clock {
+				// Colon blink on every two seconds.
+				// + On each sec divisible by two, prints an empty line
+				// + Otherwise: prints the current pixel
+				next := clock[index][line]
+				if digit == colon && sec%2 == 0 {
+					next = "   "
+				}
+				// Print the next line and,
+				// give it enough space for the next placeholder
+				fmt.Print(next, "  ")
+			}
+
+			// After each line of a placeholder, print a newline
+			fmt.Println()
+		}
+
+		// pause for 1 second
+		time.Sleep(time.Second)
 	}
 }
